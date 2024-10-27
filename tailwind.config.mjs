73506732899c8ce7a3681baba7cb0be43f5a590d
Evanston09 <evanhkim09@gmail.com>
2024-10-27 18:54:35 -0400

    A LOT of changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+        extend: {
+            transitionDuration: {
+                '2000': '2000ms',
+            }
+        }
 	},
 	plugins: [],
 }
